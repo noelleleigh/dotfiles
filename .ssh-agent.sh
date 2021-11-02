3c92ab7d8f2a1c965f11110000eae091f95ad8ee
Noelle Leigh <noelle_leigh@fastmail.com>
2021-11-01 21:50:26 -0400

    ssh-agent: fix variable references

--- .ssh-agent.sh
+++ .ssh-agent.sh
@@ -4,8 +4,8 @@
 DIR="$HOME/.config" && mkdir -p $DIR
 FILENAME="ssh-agent.env"
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > "$DIR/FILENAME"
+    ssh-agent > "$DIR/$FILENAME"
 fi
 if [[ ! "$SSH_AUTH_SOCK" ]]; then
-    source "$DIR/FILENAME" >/dev/null
+    source "$DIR/$FILENAME" >/dev/null
 fi
